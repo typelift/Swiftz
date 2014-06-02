4b05faccd98edf5a257ee995a66b450cfe7d1694
Maxwell Swadling <maxwellswadling@gmail.com>
2014-06-03 09:04:26 +1000

    init

---
+++ swiftz/swiftz.h
@@ -0,0 +1,19 @@
+//
+//  swiftz.h
+//  swiftz
+//
+//  Created by Maxwell Swadling on 3/06/2014.
+//  Copyright (c) 2014 Maxwell Swadling. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for swiftz.
+FOUNDATION_EXPORT double swiftzVersionNumber;
+
+//! Project version string for swiftz.
+FOUNDATION_EXPORT const unsigned char swiftzVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftz/PublicHeader.h>
+
+
