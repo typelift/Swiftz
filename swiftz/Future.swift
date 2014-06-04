5b8f3023ae4d3a41259210b690945b40fbf32a9b
Maxwell Swadling <maxwellswadling@gmail.com>
2014-06-04 22:56:07 +1000

    map + flatMap for future

--- swiftz/Future.swift
+++ swiftz/Future.swift
@@ -14,6 +14,7 @@ class Future<A> {
   var value: (() -> A)?
   var lock: pthread_mutex_t
   var cond: pthread_cond_t
+  let execCtx: ExecutionContext // for map
   
   init(exec: ExecutionContext, a: () -> A) {
     var mattr:pthread_mutexattr_t = pthread_mutexattr_t(__sig: 0, __opaque: (0, 0, 0, 0, 0, 0, 0, 0))
@@ -23,6 +24,7 @@ class Future<A> {
     pthread_mutexattr_settype(&mattr, PTHREAD_MUTEX_DEFAULT)
     pthread_mutex_init(&lock, &mattr)
     pthread_cond_init(&cond, nil)
+    execCtx = exec
     exec.submit({ (x: A) -> () in self.sig(x) }, a)
   }
   
@@ -40,5 +42,13 @@ class Future<A> {
     }
     pthread_mutex_unlock(&lock)
     return value!()
+  }
+  
+  func map<B>(f: A -> B) -> Future<B> {
+    return Future<B>(exec: execCtx, { f(self.result()) })
+  }
+  
+  func flatMap<B>(f: A -> Future<B>) -> Future<B> {
+    return Future<B>(exec: execCtx, { f(self.result()).result() })
   }
 }
--- swiftzTests/swiftzTests.swift
+++ swiftzTests/swiftzTests.swift
@@ -26,16 +26,19 @@ class swiftzTests: XCTestCase {
     let x: Future<Int> = Future(exec: gcdExecutionContext, {
       sleep(1)
       return 4
-
     })
-    XCTAssert(x.result() == x.result())
+    XCTAssert(x.result() == x.result(), "future")
+    XCTAssert(x.map({ $0.description }).result() == "4", "future map")
+    XCTAssert(x.flatMap({ (x: Int) -> Future<Int> in
+      return Future(exec: gcdExecutionContext, { sleep(1); return x + 1 })
+    }).result() == 5, "future flatMap")
     
-//    let x: Future<Int> = Future(exec: gcdExecutionContext, {
-//      return NSString.stringWithContentsOfURL(NSURL.URLWithString("http://github.com"), encoding: 0, error: nil)
-//    })
-//    let x1 = (x.result().lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
-//    let x2 = (x.result().lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
-//    XCTAssert(x1 == x2)
+    //    let x: Future<Int> = Future(exec: gcdExecutionContext, {
+    //      return NSString.stringWithContentsOfURL(NSURL.URLWithString("http://github.com"), encoding: 0, error: nil)
+    //    })
+    //    let x1 = (x.result().lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+    //    let x2 = (x.result().lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+    //    XCTAssert(x1 == x2)
   }
   
   func testControlBase() {
