11facccf4535e64df601e7b3e6dac4f50f6853d1
Terry Lewis <terrylewis@outlook.com>
2014-06-05 19:39:28 -0500

    Added filter for Dictionary + better formatting

--- swiftz/DictionaryExt.swift
+++ swiftz/DictionaryExt.swift
@@ -7,13 +7,23 @@
 //
 
 extension Dictionary {
-  func map<U, V>(transform: Element -> (U, V)) -> Dictionary<U, V> {
-    var d = Dictionary<U, V>()
-    for (key, value) in self {
-      switch transform(key, value) {
-      case let (k, v): d.updateValue(v, forKey: k)
-      }
+    func map<U, V>(transform: Element -> (U, V)) -> Dictionary<U, V> {
+        var d = Dictionary<U, V>()
+        for (key, value) in self {
+            switch transform(key, value) {
+            case let (k, v): d.updateValue(v, forKey: k)
+            }
+        }
+        return d
     }
-    return d
-  }
+    
+    func filter(filter: Element -> Bool) -> Dictionary {
+        var f = Dictionary()
+        for(k, v) in self {
+            if filter(k, v) {
+                f[k] = v
+            }
+        }
+        return f
+    }
 }
