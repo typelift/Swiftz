b76c4e3ec129c663ea3fd2f20a578f83f981b894
Terry Lewis <terrylewis@outlook.com>
2014-06-07 14:22:38 -0500

    Added Set struct + tests

---
+++ swiftz/Set.swift
@@ -0,0 +1,120 @@
+//
+//  Set.swift
+//  swiftz
+//
+//  Created by Terry Lewis II on 6/7/14.
+//  Copyright (c) 2014 Maxwell Swadling. All rights reserved.
+//
+
+struct Set<A: Hashable> : Sequence {
+    var bucket:Dictionary<A, Bool> = Dictionary()
+    
+    var array:Array<A> {
+    get {
+        var arr = Array<A>()
+        for (key, _) in bucket {
+            arr += key
+        }
+        return arr
+    }
+    }
+    
+    var count:Int {
+    get {
+        return bucket.count
+    }
+    }
+    
+    init(items:A...) {
+        for obj in items {
+            bucket[obj] = true
+        }
+    }
+    
+    init(array:Array<A>) {
+        for obj in array {
+            bucket[obj] = true
+        }
+    }
+    
+    func any() -> A {
+        let ar = self.array
+        let index = Int(arc4random_uniform(UInt32(ar.count)))
+        return ar[index]
+    }
+    
+    func contains(item:A) -> Bool {
+        if let c = bucket[item] {
+            return c
+        } else {
+            return false
+        }
+    }
+    
+    func member(item:A) -> A? {
+        if self.contains(item) {
+            return Optional.Some(item)
+        } else {
+            return nil
+        }
+    }
+    
+    func interectsSet(set:Set<A>) -> Bool {
+        for x in set {
+            if self.contains(x) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func union(set:Set<A>) -> Set<A> {
+        var current = self.array
+        current += set.array
+        return Set(array: current)
+    }
+    
+    func filter(f:(A -> Bool)) -> Set<A> {
+        var array = Array<A>()
+        for x in self {
+            if f(x) {
+                array += x
+            }
+        }
+        return Set(array: array)
+    }
+    
+    func map<B>(f:(A -> B)) -> Set<B> {
+        var array:Array<B> = Array()
+        for x in self {
+            array += f(x)
+        }
+        
+        return Set<B>(array: array)
+    }
+    
+    func generate() -> SetGenerator<A>  {
+        let items = self.array
+        return SetGenerator(items: items[0..items.count])
+    }
+}
+
+
+struct SetGenerator<A> : Generator {
+    mutating func next() -> A?  {
+        if items.isEmpty { return nil }
+        let ret = items[0]
+        items = items[1..items.count]
+        return ret
+    }
+    
+    var items:Slice<A>
+    
+}
+func ==<A: Equatable, B: Equatable>(lhs:Set<A>, rhs:Set<B>) -> Bool {
+    return lhs.bucket == rhs.bucket
+}
+
+func !=<A: Equatable, B: Equatable>(lhs:Set<A>, rhs:Set<B>) -> Bool {
+    return lhs.bucket != rhs.bucket
+}
---
+++ swiftzTests/SetTests.swift
@@ -0,0 +1,86 @@
+//
+//  SetTests.swift
+//  swiftz
+//
+//  Created by Terry Lewis II on 6/7/14.
+//  Copyright (c) 2014 Maxwell Swadling. All rights reserved.
+//
+
+import XCTest
+import swiftz
+class SetTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+    func testInit() {
+        let set = Set(array:[1,2,3,4,4,4,5,5,5])
+        XCTAssert(set.count == 5, "Should be 5 items")
+        
+        let newSet = Set(items:1,2,3,4,4,4,5,5,5)
+        XCTAssert(newSet.count == 5, "Should be 5 items")
+    }
+
+    func testEquality() {
+        var set1 = Set(array:[1,2,3,4,4,4,5,5,5])
+        var set2 = Set(array:[1,2,3,4,4,4,5,5,5])
+        XCTAssert(set1 == set2, "Should be equal")
+        
+        var set3 = Set(array:[1,2,5,5,5])
+        XCTAssert(set1 != set3, "Not equal")
+    }
+    
+    func testIterator() {
+        let set = Set(array:[1,2,3,4,4,4,5,5,5])
+        var iterations = 0
+        for x in set {
+            iterations++
+        }
+        XCTAssert(iterations == set.count, "should be equal")
+    }
+    
+    func testAny() {
+        let set = Set(array:[1,2,3,4,4,4,5,5,5])
+        for i in 0...3000 {
+            var any = set.any()
+            XCTAssert(any >= 1 && any <= 5, "should always be in range")
+        }
+    }
+    
+    func testUnion() {
+        let set = Set(array: [1,2,3,4,5,5,4,4,5,5])
+        let otherSet = Set(items: 6,7,8,9,10,10,1000,5600)
+        
+        let newSet = set.union(otherSet)
+        XCTAssert(newSet == Set(items: 1000, 5600, 6,7,8,9,10,1,2,3,4,5), "Should be equal")
+        
+    }
+    
+    func testFilterMap() {
+        let set = Set(array: [1,2,3,4,5,5,4,4,5,5])
+        let newSet = set.filter{$0 > 2}.map{$0 + 1}
+        XCTAssert(newSet == Set(array: [4,5,6]), "Should be equal")
+    }
+    
+    func testIntersect() {
+        let set = Set(array: [1,2,3,4,5,5,4,4,5,5])
+        XCTAssert(set.interectsSet(Set(items: 9,0,5)), "Should be true")
+    }
+    
+    func testMember() {
+        let set = Set(array: [1,2,3,4,5,5,4,4,5,5])
+        let known = set.member(4)
+        let unknown = set.member(45765)
+        
+        XCTAssert(known == Optional.Some(4), "Should be equal")
+        XCTAssert(unknown == nil, "Should be nil")
+    }
+}
