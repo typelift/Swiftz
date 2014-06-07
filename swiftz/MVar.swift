9d11390be92f2f46370c2e251a7673a899a00f22
Maxwell Swadling <maxwellswadling@gmail.com>
2014-06-07 13:13:06 +1000

    fixed mvar write

--- swiftz/MVar.swift
+++ swiftz/MVar.swift
@@ -15,18 +15,21 @@ class MVar<A> {
   var value: Optional<(() -> A)>
   
   var mutex: CMutablePointer<pthread_mutex_t>
-  var cond: CMutablePointer<pthread_cond_t>
+  var condPut: CMutablePointer<pthread_cond_t>
+  var condRead: CMutablePointer<pthread_cond_t>
   let matt: CConstPointer<pthread_mutexattr_t>
   
   init() {
     var mattr:CMutablePointer<pthread_mutexattr_t> = CMutablePointer(owner: nil, value: malloc(UInt(sizeof(pthread_mutexattr_t))).value)
     mutex = CMutablePointer(owner: nil, value: malloc(UInt(sizeof(pthread_mutex_t))).value)
-    cond = CMutablePointer(owner: nil, value: malloc(UInt(sizeof(pthread_cond_t))).value)
+    condPut = CMutablePointer(owner: nil, value: malloc(UInt(sizeof(pthread_cond_t))).value)
+    condRead = CMutablePointer(owner: nil, value: malloc(UInt(sizeof(pthread_cond_t))).value)
     pthread_mutexattr_init(mattr)
     pthread_mutexattr_settype(mattr, PTHREAD_MUTEX_RECURSIVE)
     matt = CConstPointer(nil, mattr.value)
     pthread_mutex_init(mutex, matt)
-    pthread_cond_init(cond, nil)
+    pthread_cond_init(condPut, nil)
+    pthread_cond_init(condRead, nil)
   }
   
   convenience init(a: () -> A) {
@@ -36,25 +39,29 @@ class MVar<A> {
   
   deinit {
     free(CMutableVoidPointer(owner: nil, value: mutex.value))
-    free(CMutableVoidPointer(owner: nil, value: cond.value))
+    free(CMutableVoidPointer(owner: nil, value: condPut.value))
+    free(CMutableVoidPointer(owner: nil, value: condRead.value))
     free(CMutableVoidPointer(owner: nil, value: matt.value))
   }
   
-  func put(x: A) {
-    pthread_mutex_lock(mutex)
+  func put(x: A) {pthread_mutex_lock(mutex)
+    while (value) {
+      pthread_cond_wait(condRead, mutex)
+    }
     self.value = { return x }
     pthread_mutex_unlock(mutex)
-    pthread_cond_signal(cond)
+    pthread_cond_signal(condPut)
   }
   
   func read() -> A {
     pthread_mutex_lock(mutex)
     while !(value) {
-      pthread_cond_wait(cond, mutex)
+      pthread_cond_wait(condPut, mutex)
     }
     let cp = value!()
     value = .None
     pthread_mutex_unlock(mutex)
+    pthread_cond_signal(condRead)
     return cp
   }
   
--- swiftzTests/swiftzTests.swift
+++ swiftzTests/swiftzTests.swift
@@ -49,9 +49,10 @@ class swiftzTests: XCTestCase {
   
   func testConcurrentMVar() {
     var mvar: MVar<String> = MVar()
-    let ft = Future<Void>(exec: gcdExecutionContext, { usleep(1); mvar.put("hello") })
+    let ft = Future<Void>(exec: gcdExecutionContext, { mvar.put("hello"); mvar.put("max") })
     XCTAssert(mvar.isEmpty(), "mvar is full")
     XCTAssert(mvar.read() == "hello", "mvar read")
+    XCTAssert(mvar.read() == "max", "mvar read")
     XCTAssert(mvar.isEmpty(), "mvar empty")
   }
   
