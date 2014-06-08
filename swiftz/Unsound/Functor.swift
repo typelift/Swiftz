328a1ba347a1d75eef029252c53c690c87f91d5e
joshaber <joshaber@gmail.com>
2014-06-08 00:40:53 -0700

    Added an instance of Functor for Maybe

--- swiftz/Unsound/Functor.swift
+++ swiftz/Unsound/Functor.swift
@@ -18,3 +18,48 @@ protocol Functor {
 	func fmap(fn: (A -> B)) -> FB
 }
 
+class Maybe<A: Any>: F<A> {
+	// Hack to get around IR codegen bug
+	var value: (() -> A)?
+
+	init(_ v: A) {
+		value = { return v }
+	}
+
+	init() {}
+
+	class func just(t: A) -> Maybe<A> {
+		return Maybe(t)
+	}
+
+	class func none() -> Maybe {
+		return Maybe()
+	}
+
+	func isJust() -> Bool {
+		switch value {
+		case .Some(_): return true
+		case .None: return false
+		}
+	}
+
+	func isNone() -> Bool {
+		return !isJust()
+	}
+
+	func fromJust() -> A {
+		return self.value!()
+	}
+}
+
+extension Maybe: Functor {
+	typealias B = Any
+	func fmap(fn: (A -> B)) -> Maybe<B> {
+		if self.isJust() {
+			let b: B = fn(self.fromJust())
+			return Maybe<B>.just(b);
+		} else {
+			return Maybe<B>.none()
+		}
+	}
+}
