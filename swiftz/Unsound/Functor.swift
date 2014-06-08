8192cc28e2a644d7c27edb6592a6b0dba605139f
joshaber <joshaber@gmail.com>
2014-06-08 00:40:07 -0700

    Added Functor?

--- swiftz/Unsound/Functor.swift
+++ swiftz/Unsound/Functor.swift
@@ -2,8 +2,19 @@
 //  Functor.swift
 //  swiftz
 //
-//  Created by Maxwell Swadling on 6/06/2014.
-//  Copyright (c) 2014 Maxwell Swadling. All rights reserved.
+//  Created by Josh Abernathy on 6/7/2014.
+//  Copyright (c) 2014 Josh Abernathy. All rights reserved.
 //
 
 import Foundation
+
+class F<A> {
+}
+
+protocol Functor {
+	typealias A
+	typealias B
+	typealias FB = F<B>
+	func fmap(fn: (A -> B)) -> FB
+}
+
