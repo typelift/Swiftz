f1d7c4ea563ce05bd0ca71b2c9353e2854a09863
Maxwell Swadling <maxwellswadling@gmail.com>
2014-06-09 15:11:05 +1000

    added function composition

--- swiftz/Base.swift
+++ swiftz/Base.swift
@@ -16,6 +16,14 @@ func identity<A>(a: A) -> A {
   return a;
 }
 
+func comp<A, B, C>(f: B -> C) -> (A -> B) -> A -> C {
+  return { (g: (A -> B)) -> A -> C in
+    return { (a: A) -> C in
+      return f(g(a))
+    }
+  }
+}
+
 operator infix |> {
   associativity left
 }
