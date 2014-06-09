d24c793922f650a8a5b6cc75a8bda669ab2c9563
Andrew Cobb <andrew.cobb@gmail.com>
2014-06-08 23:46:30 -0600

    List now has Functor instance and some more conveniences

--- swiftz/List.swift
+++ swiftz/List.swift
@@ -6,15 +6,33 @@
 //  Copyright (c) 2014 Maxwell Swadling. All rights reserved.
 //
 
+class Box<T> {
+    let value : () -> T
+    init(_ value : T) {
+        self.value = { value }
+    }
+}
+
 enum List<A> {
-    case Nil()
-    case Cons(A, OnHeap<List<A>>)
+    case Nil
+    case Cons(A, Box<List<A>>)
 
     init() {
-        self = .Nil()
+        self = .Nil
     }
     init(_ head : A, _ tail : List<A>) {
-        self = .Cons(head, OnHeap(tail))
+        self = .Cons(head, Box(tail))
+    }
+}
+
+func==<A : Equatable>(lhs : List<A>, rhs : List<A>) -> Bool {
+    switch (lhs, rhs) {
+    case (.Nil, .Nil):
+        return true
+    case let (.Cons(lHead, lTail), .Cons(rHead, rTail)):
+        return lHead == rHead && lTail.value() == rTail.value()
+    default:
+        return false
     }
 }
 
@@ -28,12 +46,35 @@ extension List : ArrayLiteralConvertible {
         }
         var l = List()
         for x in xs.reverse() {
-            l = List<A>(x, l)
+            l = List(x, l)
         }
         return l
     }
 
     static func convertFromArrayLiteral(elements: A...) -> List<A> {
         return fromSeq(elements)
+    }
+}
+
+extension List : Printable {
+    var description : String {
+    switch self {
+    case .Nil:
+        return "()"
+    case let .Cons(head, tail):
+        return "(\(head) . \(tail.value()))"
+        }
+    }
+}
+
+struct ListF<A, B> : Functor {
+    let l : List<A>
+    func fmap(fn : (A -> B)) -> List<B> {
+        switch l {
+        case .Nil:
+           return List()
+        case let .Cons(head, tail):
+            return List(fn(head), ListF(l: tail.value()).fmap(fn))
+        }
     }
 }
