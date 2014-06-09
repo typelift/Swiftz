820d00e26d30010228e120a5e433989cc525e21d
Andrew Cobb <andrew.cobb@gmail.com>
2014-06-08 23:01:48 -0600

    List now implements ArrayLiteralConvertible

--- swiftz/List.swift
+++ swiftz/List.swift
@@ -9,11 +9,31 @@
 enum List<A> {
     case Nil()
     case Cons(A, OnHeap<List<A>>)
-    
+
     init() {
         self = .Nil()
     }
     init(_ head : A, _ tail : List<A>) {
         self = .Cons(head, OnHeap(tail))
+    }
+}
+
+extension List : ArrayLiteralConvertible {
+    static func fromSeq<S : Sequence where S.GeneratorType.Element == A>(s : S) -> List<A> {
+        // For some reason, everything simpler seems to crash the compiler
+        var xs : A[] = []
+        var g = s.generate()
+        while let x : A = g.next() {
+            xs += x
+        }
+        var l = List()
+        for x in xs.reverse() {
+            l = List<A>(x, l)
+        }
+        return l
+    }
+
+    static func convertFromArrayLiteral(elements: A...) -> List<A> {
+        return fromSeq(elements)
     }
 }
