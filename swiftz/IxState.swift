8510290a59fc906ad829a3c3adafe2338e4990b1
Alexander Altman <alexanderaltman@me.com>
2014-06-11 17:10:52 -0700

    Lenses and indexed state

---
+++ swiftz/IxState.swift
@@ -0,0 +1,105 @@
+//
+//  IxState.swift
+//  swiftz
+//
+//  Created by Alexander Ronald Altman on 6/11/14.
+//  Copyright (c) 2014 Maxwell Swadling. All rights reserved.
+//
+
+import Foundation
+
+struct IxState<I, O, A> {
+     let run: I -> (A, O)
+
+     init(_ run: I -> (A, O)) {
+          self.run = run
+     }
+
+     func eval(s: I) -> A {
+          return run(s).0
+     }
+
+     func exec(s: I) -> O {
+          return run(s).1
+     }
+
+     func map<B>(f: A -> B) -> IxState<I, O, B> {
+          return f <^> self
+     }
+
+     func contramap<H>(f: H -> I) -> IxState<H, O, A> {
+          return f <!> self
+     }
+
+     func imap<P>(f: O -> P) -> IxState<I, P, A> {
+          return f <^^> self
+     }
+
+     func ap<E, B>(f: IxState<E, I, A -> B>) -> IxState<E, O, B> {
+          return f <*> self
+     }
+
+     func flatMap<E, B>(f: A -> IxState<O, E, B>) -> IxState<I, E, B> {
+          return self >>= f
+     }
+}
+
+func pure<I, A>(x: A) -> IxState<I, I, A> {
+     return IxState { (x, $0) }
+}
+
+func <^><I, O, A, B>(f: A -> B, a: IxState<I, O, A>) -> IxState<I, O, B> {
+     return IxState { s1 in
+          let (x, s2) = a.run(s1)
+          return (f(x), s2)
+     }
+}
+
+func <!><H, I, O, A>(f: H -> I, a: IxState<I, O, A>) -> IxState<H, O, A> {
+     return IxState { a.run(f($0)) }
+}
+
+func <^^><I, O, P, A>(f: O -> P, a: IxState<I, O, A>) -> IxState<I, P, A> {
+     return IxState { s1 in
+          let (x, s2) = a.run(s1)
+          return (x, f(s2))
+     }
+}
+
+func <*><I, J, O, A, B>(f: IxState<I, J, A -> B>, a: IxState<J, O, A>) -> IxState<I, O, B> {
+     return IxState { s1 in
+          let (g, s2) = f.run(s1)
+          let (x, s3) = a.run(s2)
+          return (g(x), s3)
+     }
+}
+
+func >>=<I, J, O, A, B>(a: IxState<I, J, A>, f: A -> IxState<J, O, B>) -> IxState<I, O, B> {
+     return IxState { s1 in
+          let (x, s2) = a.run(s1)
+          return f(x).run(s2)
+     }
+}
+
+func join<I, J, O, A>(a: IxState<I, J, IxState<J, O, A>>) -> IxState<I, O, A> {
+     return IxState { s1 in
+          let (b, s2) = a.run(s1)
+          return b.run(s2)
+     }
+}
+
+func get<I>() -> IxState<I, I, I> {
+     return IxState { ($0, $0) }
+}
+
+func gets<I, A>(f: I -> A) -> IxState<I, I, A> {
+     return IxState { (f($0), $0) }
+}
+
+func put<I, O>(s: O) -> IxState<I, O, ()> {
+     return IxState { _ in ((), s) }
+}
+
+func modify<I, O>(f: I -> O) -> IxState<I, O, ()> {
+     return IxState { ((), f($0)) }
+}
--- swiftz/Lens.swift
+++ swiftz/Lens.swift
@@ -8,19 +8,51 @@
 
 import Foundation
 
-class Lens<A, B> {
-  typealias LensConst = (B -> Const<B, A>) -> A -> Const<B, A>
-  typealias LensId = (B -> Id<B>) -> A -> Id<A>
-  
-  class func get(lens: LensConst, _ a: A) -> B {
-    return (lens({ (b: B) -> Const<B, A> in return Const<B, A>(b) })(a)).runConst()
-  }
-  
-  class func set(lens: LensId, _ b: B, _ a: A) -> A {
-    return lens({ (_: B) -> Id<B> in return Id<B>(b) })(a).runId()
-  }
-  
-  class func modify(lens: LensId, _ f: (B -> B), _ a: A) -> A {
-    return lens({ (b: B) -> Id<B> in return Id<B>(f(b)) })(a).runId()
-  }
+class Lens<S, T, A, B> {
+     let run: S -> (A, B -> T)
+
+     init(_ run: S -> (A, B -> T)) {
+          self.run = run
+     }
+
+     convenience init(get: S -> A, set: (S, B) -> T) {
+          self.init({ v in (get(v), { set(v, $0) }) })
+     }
+
+     convenience init(get: S -> A, modify: (S, A -> B) -> T) {
+          self.init(get: get, set: { v, x in modify(v, { _ in x }) })
+     }
+
+     func get(v: S) -> A {
+          return run(v).0
+     }
+
+     func set(v: S, _ x: B) -> T {
+          return (run(v).1)(x)
+     }
+
+     func modify(v: S, _ f: A -> B) -> T {
+          let (x, g) = run(v)
+          return g(f(x))
+     }
+
+     func zoom<X>(a: IxState<A, B, X>) -> IxState<S, T, X> {
+          return IxState { s1 in
+               let (s2, f) = self.run(s1)
+               let (x, s3) = a.run(s2)
+               return (x, f(s3))
+          }
+     }
+}
+
+func identity<S, T>() -> Lens<S, T, S, T> {
+     return Lens { ($0, identity) }
+}
+
+func comp<S, T, I, J, A, B>(l1: Lens<S, T, I, J>)(l2: Lens<I, J, A, B>) -> Lens<S, T, A, B> {
+     return Lens { v in
+          let (k, f1) = l1.run(v)
+          let (x, f2) = l2.run(k)
+          return (x, { f1(f2($0)) })
+     }
 }
