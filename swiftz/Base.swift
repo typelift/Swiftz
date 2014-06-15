ecd367658b5662760ca2d2d1d706cd4c3e3ace66
Maxwell Swadling <maxwellswadling@gmail.com>
2014-06-15 16:55:21 +1000

    reverted formatting changes

--- swiftz/Base.swift
+++ swiftz/Base.swift
@@ -9,40 +9,40 @@
 // the building blocks of FP
 
 func flip<A, B, C>(f: ((A, B) -> C), b: B, a: A) -> C {
-    return f(a, b)
+  return f(a, b)
 }
 
 func identity<A>(a: A) -> A {
-    return a;
+  return a;
 }
 
 // Function composition
 operator infix .... {
-  associativity right
+associativity right
 }
 
 func ....<A, B, C>(f: B -> C, g: A -> B) -> A -> C {
-    return { (a: A) -> C in
-        return f(g(a))
-    }
+  return { (a: A) -> C in
+    return f(g(a))
+  }
 }
 
 operator infix • {
-  associativity right
+associativity right
 }
 
 func •<A, B, C>(f: B -> C, g: A -> B) -> A -> C {
-    return { (a: A) -> C in
-        return f(g(a))
-    }
+  return { (a: A) -> C in
+    return f(g(a))
+  }
 }
 
 func comp<A, B, C>(f: B -> C) -> (A -> B) -> A -> C {
-    return { (g: (A -> B)) -> A -> C in
-        return { (a: A) -> C in
-            return f(g(a))
-        }
+  return { (g: (A -> B)) -> A -> C in
+    return { (a: A) -> C in
+      return f(g(a))
     }
+  }
 }
 
 operator infix |> {
@@ -56,11 +56,11 @@ associativity left
 
 // Thrush
 func |><A, B>(a: A, f: A -> B) -> B {
-    return f(a)
+  return f(a)
 }
 
 func ▹<A, B>(a: A, f: A -> B) -> B {
-    return f(a)
+  return f(a)
 }
 
 // Unsafe tap
@@ -86,11 +86,11 @@ associativity left
 }
 
 operator infix <^^> {
-  associativity left
+associativity left
 }
 
 operator infix <!> {
-  associativity left
+associativity left
 }
 
 operator infix <*> {
@@ -98,72 +98,72 @@ associativity left
 }
 
 operator infix =>> {
-  associativity left
+associativity left
 }
 
 // Optional
 
 func pure<A>(a: A) -> A? {
-    return a
+  return a
 }
 
 func <^><A, B>(f: A -> B, a: A?) -> B? {
-    if let x = a {
-        return (f(x))
-    } else {
-        return .None
-    }
+  if let x = a {
+    return (f(x))
+  } else {
+    return .None
+  }
 }
 
 func <*><A, B>(f: (A -> B)?, a: A?) -> B? {
-    if f && a {
-        return (f!(a!))
-    } else {
-        return .None
-    }
+  if f && a {
+    return (f!(a!))
+  } else {
+    return .None
+  }
 }
 
 // the "if the arg is Some, apply the function that returns an optional
 // value and if the arg is None, just return None" function.
 func >>=<A, B>(a: A?, f: A -> B?) -> B? {
-    if let x = a {
-        return f(x)
-    } else {
-        return .None
-    }
+  if let x = a {
+    return f(x)
+  } else {
+    return .None
+  }
 }
 
 // array
 
 func pure<A>(a: A) -> Array<A> {
-    var v = Array<A>()
-    v.append(a)
-    return v
+  var v = Array<A>()
+  v.append(a)
+  return v
 }
 
 // Note well! This is not map! Map mutates the array, this copies it.
 func <^><A, B>(f: A -> B, a: Array<A>) -> Array<B> {
-    var xs = Array<B>()
-    for x in a {
-        xs.append(f(x))
-    }
-    return xs
+  var xs = Array<B>()
+  for x in a {
+    xs.append(f(x))
+  }
+  return xs
 }
 
 func <*><A, B>(f: Array<A -> B>, a: Array<A>) -> Array<B> {
-    var re = Array<B>()
-    for g in f {
-        for h in a {
-            re.append(g(h))
-        }
+  var re = Array<B>()
+  for g in f {
+    for h in a {
+      re.append(g(h))
     }
-    return re
+  }
+  return re
 }
 
 func >>=<A, B>(a: Array<A>, f: A -> Array<B>) -> Array<B> {
-    var re = Array<B>()
-    for x in a {
-        re.extend(f(x))
-    }
-    return re
+  var re = Array<B>()
+  for x in a {
+    re.extend(f(x))
+  }
+  return re
 }
