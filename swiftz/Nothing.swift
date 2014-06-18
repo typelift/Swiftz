3098e975ab7ed56fcc112e8bc23a04329b33d166
Maxwell Swadling <maxwellswadling@gmail.com>
2014-06-19 00:31:18 +1000

    added Nothing

---
+++ swiftz/Nothing.swift
@@ -0,0 +1,13 @@
+//
+//  Nothing.swift
+//  swiftz
+//
+//  Created by Maxwell Swadling on 18/06/2014.
+//  Copyright (c) 2014 Maxwell Swadling. All rights reserved.
+//
+
+import Foundation
+
+enum Nothing {
+  
+}
