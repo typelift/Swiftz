735a5a73c3da6342fa588911a98aaaafd4d1869c
Max Cantor (MBPr) <i@cantor.mx>
2014-07-28 22:54:14 -0700

    Make Future.sig public

--- swiftz/MaybeFunctor.swift
+++ swiftz/MaybeFunctor.swift
@@ -67,7 +67,7 @@ public struct MaybeF<A, B>: Functor, Applicative {
   // functor
   public let m: Maybe<A>
     
-  public init(m: Maybe<A>) {
+  public init(_ m: Maybe<A>) {
     self.m = m
   }
   public func fmap(f: (A -> B)) -> Maybe<B> {
@@ -84,10 +84,10 @@ public struct MaybeF<A, B>: Functor, Applicative {
     return Maybe<A>.just(a)
   }
   
-  public func ap(f: Maybe<A -> B>) -> Maybe<B>  {
-    if f.isJust() {
-      let fn: (A -> B) = f.fromJust()
-      return MaybeF(m: m).fmap(fn)
+  public func ap(fn: Maybe<A -> B>) -> Maybe<B>  {
+    if fn.isJust() {
+      let f: (A -> B) = fn.fromJust()
+      return MaybeF(m).fmap(f)
     } else {
       return Maybe<B>.none()
     }
