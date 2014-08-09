f15ceac8ec8be6bc2218a291d1af07625d2514ab
Terry Lewis <terrylewis@outlook.com>
2014-08-09 16:26:10 -0500

    Added more curry functions

--- swiftz/Curry.swift
+++ swiftz/Curry.swift
@@ -37,3 +37,28 @@ public func uncurry<A, B, C, D, E>(f: (A -> (B -> (C -> (D -> E)))), abcd: (A, B
   case let (a, b, c, d): return (f(a)(b)(c)(d))
   }
 }
+
+public func curry<A,B,C>(f: (A,B) -> C) -> A -> B -> C {
+    return { a in { b in f(a,b) } }
+}
+
+public func curry<A,B,C,D>(f: (A,B,C) -> D) -> A -> B -> C -> D {
+    return { a in { b in {c in f(a,b,c) } } }
+}
+
+public func curry<A,B,C,D,E>(f: (A,B,C,D) -> E) -> A -> B -> C -> D -> E {
+    return { a in { b in { c in { d in f(a,b,c,d) } } } }
+}
+
+public func uncurry<A,B,C>(f: A -> B -> C) -> (A,B) -> C {
+    return {a,b in f(a)(b) }
+}
+
+public func uncurry<A,B,C,D>(f: A -> B -> C -> D) -> (A,B,C) -> D {
+    return {a,b,c in f(a)(b)(c) }
+}
+
+public func uncurry<A,B,C,D,E>(f: A -> B -> C -> D -> E) -> (A,B,C,D) -> E {
+    return {a,b,c,d in f(a)(b)(c)(d) }
+}
+
