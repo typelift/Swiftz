da9c64df6759b5024dfccf2c21db5e35e7896ba8
Robert Widmann <devteam.codafi@gmail.com>
2014-11-24 23:16:47 -0700

    Remove or rename ambiguous operators

--- swiftz_core/swiftz_core/Sections.swift
+++ swiftz_core/swiftz_core/Sections.swift
@@ -485,20 +485,12 @@ public postfix func |(lhs: UInt32) -> UInt32 -> UInt32 {
 prefix operator ?? {}
 postfix operator ?? {}
 
-public prefix func ??<T>(defaultValue: @autoclosure () -> T) -> T? -> T {
+public prefix func ??<T>(defaultValue: T) -> T? -> T {
 	return { optional in optional ?? defaultValue }
 }
 
-public postfix func ??<T>(optional: T?) -> T -> T {
-	return { defaultValue in optional ?? defaultValue }
-}
-
-public prefix func ??<T>(defaultValue: @autoclosure () -> T?) -> T? -> T? {
+public prefix func ??<T>(defaultValue: T?) -> T? -> T? {
 	return { optional in optional ?? defaultValue }
-}
-
-public postfix func ??<T>(optional: T?) -> T? -> T? {
-	return { defaultValue in optional ?? defaultValue }
 }
 
 prefix operator % {}
@@ -889,7 +881,7 @@ public postfix func +<T : Strideable>(lhs: T) -> T.Stride -> T {
 prefix operator - {}
 postfix operator - {}
 
-public prefix func -(rhs: Int64) -> Int64 -> Int64 {
+public prefix func ~(rhs: Int64) -> Int64 -> Int64 {
 	return { lhs in lhs - rhs }
 }
 
@@ -897,7 +889,7 @@ public postfix func -(lhs: Int64) -> Int64 -> Int64 {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -(rhs: UInt8) -> UInt8 -> UInt8 {
+public prefix func ~(rhs: UInt8) -> UInt8 -> UInt8 {
 	return { lhs in lhs - rhs }
 }
 
@@ -905,7 +897,7 @@ public postfix func -(lhs: UInt8) -> UInt8 -> UInt8 {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -(rhs: Int8) -> Int8 -> Int8 {
+public prefix func ~(rhs: Int8) -> Int8 -> Int8 {
 	return { lhs in lhs - rhs }
 }
 
@@ -913,7 +905,7 @@ public postfix func -(lhs: Int8) -> Int8 -> Int8 {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -(rhs: UInt32) -> UInt32 -> UInt32 {
+public prefix func ~(rhs: UInt32) -> UInt32 -> UInt32 {
 	return { lhs in lhs - rhs }
 }
 
@@ -921,7 +913,7 @@ public postfix func -(lhs: UInt32) -> UInt32 -> UInt32 {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -(rhs: UInt16) -> UInt16 -> UInt16 {
+public prefix func ~(rhs: UInt16) -> UInt16 -> UInt16 {
 	return { lhs in lhs - rhs }
 }
 
@@ -929,7 +921,7 @@ public postfix func -(lhs: UInt16) -> UInt16 -> UInt16 {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -<T : _IntegerArithmeticType>(rhs: T) -> T -> T {
+public prefix func ~<T : _IntegerArithmeticType>(rhs: T) -> T -> T {
 	return { lhs in lhs - rhs }
 }
 
@@ -937,7 +929,7 @@ public postfix func -<T : _IntegerArithmeticType>(lhs: T) -> T -> T {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -(rhs: Swift.Float80) -> Swift.Float80 -> Swift.Float80 {
+public prefix func ~(rhs: Swift.Float80) -> Swift.Float80 -> Swift.Float80 {
 	return { lhs in lhs - rhs }
 }
 
@@ -945,7 +937,7 @@ public postfix func -(lhs: Swift.Float80) -> Swift.Float80 -> Swift.Float80 {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -(rhs: Double) -> Double -> Double {
+public prefix func ~(rhs: Double) -> Double -> Double {
 	return { lhs in lhs - rhs }
 }
 
@@ -953,7 +945,7 @@ public postfix func -(lhs: Double) -> Double -> Double {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -(rhs: Float) -> Float -> Float {
+public prefix func ~(rhs: Float) -> Float -> Float {
 	return { lhs in lhs - rhs }
 }
 
@@ -961,7 +953,7 @@ public postfix func -(lhs: Float) -> Float -> Float {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -(rhs: Int) -> Int -> Int {
+public prefix func ~(rhs: Int) -> Int -> Int {
 	return { lhs in lhs - rhs }
 }
 
@@ -969,7 +961,7 @@ public postfix func -(lhs: Int) -> Int -> Int {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -(rhs: Int16) -> Int16 -> Int16 {
+public prefix func ~(rhs: Int16) -> Int16 -> Int16 {
 	return { lhs in lhs - rhs }
 }
 
@@ -977,7 +969,7 @@ public postfix func -(lhs: Int16) -> Int16 -> Int16 {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -(rhs: Int32) -> Int32 -> Int32 {
+public prefix func ~(rhs: Int32) -> Int32 -> Int32 {
 	return { lhs in lhs - rhs }
 }
 
@@ -985,7 +977,7 @@ public postfix func -(lhs: Int32) -> Int32 -> Int32 {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -(rhs: UInt64) -> UInt64 -> UInt64 {
+public prefix func ~(rhs: UInt64) -> UInt64 -> UInt64 {
 	return { lhs in lhs - rhs }
 }
 
@@ -993,7 +985,7 @@ public postfix func -(lhs: UInt64) -> UInt64 -> UInt64 {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -(rhs: UInt) -> UInt -> UInt {
+public prefix func ~(rhs: UInt) -> UInt -> UInt {
 	return { lhs in lhs - rhs }
 }
 
@@ -1001,7 +993,7 @@ public postfix func -(lhs: UInt) -> UInt -> UInt {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -<T>(rhs: Int) -> UnsafePointer<T> -> UnsafePointer<T> {
+public prefix func ~<T>(rhs: Int) -> UnsafePointer<T> -> UnsafePointer<T> {
 	return { lhs in lhs - rhs }
 }
 
@@ -1009,7 +1001,7 @@ public postfix func -<T>(lhs: UnsafePointer<T>) -> Int -> UnsafePointer<T> {
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -<T>(lhs: UnsafePointer<T>) -> Int -> UnsafePointer<T> {
+public prefix func ~<T>(lhs: UnsafePointer<T>) -> Int -> UnsafePointer<T> {
 	return { rhs in lhs - rhs }
 }
 
@@ -1017,7 +1009,7 @@ public postfix func -<T>(rhs: Int) -> UnsafePointer<T> -> UnsafePointer<T> {
 	return { lhs in lhs - rhs }
 }
 
-public prefix func -<T>(lhs: UnsafeMutablePointer<T>) -> Int -> UnsafeMutablePointer<T> {
+public prefix func ~<T>(lhs: UnsafeMutablePointer<T>) -> Int -> UnsafeMutablePointer<T> {
 	return { rhs in lhs - rhs }
 }
 
@@ -1025,7 +1017,7 @@ public postfix func -<T>(rhs: Int) -> UnsafeMutablePointer<T> -> UnsafeMutablePo
 	return { lhs in lhs - rhs }
 }
 
-public prefix func -<T>(rhs: Int) -> UnsafeMutablePointer<T> -> UnsafeMutablePointer<T> {
+public prefix func ~<T>(rhs: Int) -> UnsafeMutablePointer<T> -> UnsafeMutablePointer<T> {
 	return { lhs in lhs - rhs }
 }
 
@@ -1033,7 +1025,7 @@ public postfix func -<T>(lhs: UnsafeMutablePointer<T>) -> Int -> UnsafeMutablePo
 	return { rhs in lhs - rhs }
 }
 
-public prefix func -<T : Strideable>(rhs: T.Stride) -> T -> T {
+public prefix func ~<T : Strideable>(rhs: T.Stride) -> T -> T {
 	return { lhs in lhs - rhs }
 }
 
