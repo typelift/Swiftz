960d75b2ef2c3742c5a7bd0358ee593ec3c0d933
Robert Widmann <devteam.codafi@gmail.com>
2014-11-25 11:55:53 -0700

    Scope first

--- swiftz/Sections.swift
+++ swiftz/Sections.swift
@@ -72,67 +72,67 @@ public func >>-<I, A, B>(f: A -> (I -> B), k: I -> A) -> (I -> B) {
 
 /// MARK: Sections
 
-prefix public func •<A, B, C>(g : A -> B) -> (B -> C) -> A -> C {
+public prefix func •<A, B, C>(g : A -> B) -> (B -> C) -> A -> C {
 	return { f in { a in f(g(a)) } }
 }
 
-postfix public func •<A, B, C>(f: B -> C) -> (A -> B) -> A -> C {
+public postfix func •<A, B, C>(f: B -> C) -> (A -> B) -> A -> C {
 	return { g in { a in f(g(a)) } }
 }
 
-prefix public func §<A, B>(a: A) -> (A -> B) -> B {
+public prefix func §<A, B>(a: A) -> (A -> B) -> B {
 	return { f in f(a) }
 }
 
-postfix public func §<A, B>(f: A -> B) -> A -> B {
+public postfix func §<A, B>(f: A -> B) -> A -> B {
 	return { a in f(a) }
 }
 
-prefix public func <|<A, B>(a: A) -> (A -> B) -> B {
+public prefix func <|<A, B>(a: A) -> (A -> B) -> B {
 	return { f in f(a) }
 }
 
-postfix public func <|<A, B>(f: A -> B) -> A -> B {
+public postfix func <|<A, B>(f: A -> B) -> A -> B {
 	return { a in f(a) }
 }
 
-prefix public func |><A, B>(f: A -> B) -> A -> B {
+public prefix func |><A, B>(f: A -> B) -> A -> B {
 	return { a in f(a) }
 }
 
-postfix public func |><A, B>(a: A) -> (A -> B) -> B {
+public postfix func |><A, B>(a: A) -> (A -> B) -> B {
 	return { f in f(a) }
 }
 
-prefix public func <^><I, A, B>(k: I -> A) -> (A -> B) -> (I -> B) {
+public prefix func <^><I, A, B>(k: I -> A) -> (A -> B) -> (I -> B) {
 	return { f in { x in f(k(x)) } }
 }
 
-postfix public func <^><I, A, B>(f: A -> B) -> (I -> A) -> (I -> B) {
+public postfix func <^><I, A, B>(f: A -> B) -> (I -> A) -> (I -> B) {
 	return { k in { x in f(k(x)) } }
 }
 
-prefix public func <!><I, J, A>(k: I -> A) -> (J -> I) -> (J -> A) {
+public prefix func <!><I, J, A>(k: I -> A) -> (J -> I) -> (J -> A) {
 	return { f in { x in k(f(x)) } }
 }
 
-postfix public func <!><I, J, A>(f: J -> I) -> (I -> A) -> (J -> A) {
+public postfix func <!><I, J, A>(f: J -> I) -> (I -> A) -> (J -> A) {
 	return { k in { x in k(f(x)) } }
 }
 
-prefix public func <*><I, A, B>(k: I -> A) -> (I -> (A -> B)) -> (I -> B) {
+public prefix func <*><I, A, B>(k: I -> A) -> (I -> (A -> B)) -> (I -> B) {
 	return { f in { x in f(x)(k(x)) } }
 }
 
-postfix public func <*><I, A, B>(f: I -> (A -> B)) -> (I -> A) -> (I -> B) {
+public postfix func <*><I, A, B>(f: I -> (A -> B)) -> (I -> A) -> (I -> B) {
 	return { k in { x in f(x)(k(x)) } }
 }
 
-prefix public func >>-<I, A, B>(k: I -> A) -> (A -> (I -> B)) -> (I -> B) {
+public prefix func >>-<I, A, B>(k: I -> A) -> (A -> (I -> B)) -> (I -> B) {
 	return { f in { x in f(k(x))(x) } }
 }
 
-postfix public func >>-<I, A, B>(f: A -> (I -> B)) -> (I -> A) -> (I -> B) {
+public postfix func >>-<I, A, B>(f: A -> (I -> B)) -> (I -> A) -> (I -> B) {
 	return { k in { x in f(k(x))(x) } }
 }
 
