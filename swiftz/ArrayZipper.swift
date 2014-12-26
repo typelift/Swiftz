7fbd8ea87d4ac76693f651236ce6272b3e3b39d6
Robert Widmann <devteam.codafi@gmail.com>
2014-12-26 15:38:44 -0700

    Prime Zipper for a Comonad instance

--- swiftz/ArrayZipper.swift
+++ swiftz/ArrayZipper.swift
@@ -6,14 +6,15 @@
 //  Copyright (c) 2014 Maxwell Swadling. All rights reserved.
 //
 
-/// A cursor for arrays.
+/// An array with a zipper.  Zippers are convenient ways of traversing and modifying the parts of a
+/// structure using a cursor to focus on individual parts of the structure.
 public struct ArrayZipper<A> : ArrayLiteralConvertible {
 	typealias Element = A
 
-	public let values: [A]
-	public let position: Int
+	public let values : [A]
+	public let position : Int
 
-	public init(_ values: [A] = [], _ position: Int = 0) {
+	public init(_ values : [A] = [], _ position : Int = 0) {
 		if position < 0 {
 			self.position = 0
 		} else if position >= values.count {
@@ -24,43 +25,39 @@ public struct ArrayZipper<A> : ArrayLiteralConvertible {
 		self.values = values
 	}
 
-	public init(arrayLiteral elements: Element...) {
+	public init(arrayLiteral elements : Element...) {
 		self.init(elements, 0)
 	}
 
-	public func map<B>(f: A -> B) -> ArrayZipper<B> {
+	public func map<B>(f : A -> B) -> ArrayZipper<B> {
 		return f <^> self
 	}
 
-	public func dup() -> ArrayZipper<ArrayZipper<A>> {
-		return duplicate(self)
+	public func extract(xz : ArrayZipper<A>) -> A {
+		return xz.values[xz.position]
 	}
 
-	public func extend<B>(f: ArrayZipper<A> -> B) -> ArrayZipper<B> {
+	public func duplicate() -> ArrayZipper<ArrayZipper<A>> {
+		return ArrayZipper<ArrayZipper<A>>((0 ..< self.values.count).map { ArrayZipper(self.values, $0) }, self.position)
+	}
+
+	public func extend<B>(f : ArrayZipper<A> -> B) -> ArrayZipper<B> {
 		return self ->> f
 	}
 
-	public func move(n: Int = 1) -> ArrayZipper<A> {
+	public func move(n : Int = 1) -> ArrayZipper<A> {
 		return ArrayZipper(values, position + n)
 	}
 
-	public func moveTo(pos: Int) -> ArrayZipper<A> {
+	public func moveTo(pos : Int) -> ArrayZipper<A> {
 		return ArrayZipper(values, pos)
 	}
 }
 
-public func extract<A>(xz: ArrayZipper<A>) -> A {
-	return xz.values[xz.position]
-}
-
-public func <^><A, B>(f: A -> B, xz: ArrayZipper<A>) -> ArrayZipper<B> {
+public func <^><A, B>(f : A -> B, xz : ArrayZipper<A>) -> ArrayZipper<B> {
 	return ArrayZipper(f <^> xz.values, xz.position)
 }
 
-public func duplicate<A>(xz: ArrayZipper<A>) -> ArrayZipper<ArrayZipper<A>> {
-	return ArrayZipper((0 ..< xz.values.count).map { ArrayZipper(xz.values, $0) }, xz.position)
-}
-
-public func ->><A, B>(xz: ArrayZipper<A>, f: ArrayZipper<A> -> B) -> ArrayZipper<B> {
+public func ->><A, B>(xz : ArrayZipper<A>, f: ArrayZipper<A> -> B) -> ArrayZipper<B> {
 	return ArrayZipper((0 ..< xz.values.count).map { f(ArrayZipper(xz.values, $0)) }, xz.position)
 }
--- swiftz/Comonad.swift
+++ swiftz/Comonad.swift
@@ -6,15 +6,19 @@
 //  Copyright (c) 2014 Maxwell Swadling. All rights reserved.
 //
 
-/// A Comonad is the categorical dual to a Monad.  If a Monad is the model of a computation that
-/// produces a value of type T, a Comonad is a value produced from a context.
+/// A Comonad is the categorical dual to a Monad.
 ///
 /// "A comonoid in the monoidal category of endofunctors"
 public protocol Comonad : Functor {
+	typealias FFA = K1<Self>
+
 	/// Uses the surrounding context to compute a value.
 	func extract() -> A
 
-	/// Duplicates the surrounding context and computes a value from it while remaining in the 
-	/// original context.
+	/// Duplicates the surrounding comonadic context and embeds the reciever in it.
+	func duplicate() -> FFA
+
+	/// Duplicates the surrounding comonadic context of the reciever and applies a function to the
+	/// reciever to yield a new value in that context.
 	func extend(fab : Self -> B) -> FB
 }
