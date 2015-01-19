936e553f6a34fa6ee3f5ede8230a2688523fffc4
Robert Widmann <devteam.codafi@gmail.com>
2015-01-19 14:24:05 -0700

    Remove extraneous structure

--- swiftzTests/PartyExample.swift
+++ swiftzTests/PartyExample.swift
@@ -11,20 +11,20 @@ import swiftz
 // A party has a host, who is a user.
 // A lens example
 public class Party {
-  let host: User
-  public init(h: User) {
-    host = h
-  }
+	let host: User
+	public init(h: User) {
+		host = h
+	}
 
-  class func lpartyHost() -> Lens<Party, Party, User, User> {
-    let getter = { (party: Party) -> User in
-      party.host
-    }
+	class func lpartyHost() -> Lens<Party, Party, User, User> {
+		let getter = { (party: Party) -> User in
+			party.host
+		}
 
-    let setter = { (party: Party, host: User) -> Party in
-      Party(h: host)
-    }
+		let setter = { (party: Party, host: User) -> Party in
+			Party(h: host)
+		}
 
-    return Lens(get: getter, set: setter)
-  }
+		return Lens(get: getter, set: setter)
+	}
 }
