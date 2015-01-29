983e76124ea725da3d247ed2b3606185f36c16c6
Alexander Altman <alexanderaltman@me.com>
2015-01-28 19:12:32 -0800

    Revamp `Num`, `Semigroup`, and `Monoid`
    They are now proper traits that use `Self`, rather than
    explicitly-passed dictionaries.

--- Swiftz/Monoid.swift
+++ Swiftz/Monoid.swift
@@ -9,48 +9,111 @@
 /// A Monoid is a Semigroup that distinguishes an identity element.
 public protocol Monoid : Semigroup {
 	/// The identity element of the Monoid.
-	func mzero() -> M
+    class var mzero: Self { get }
 }
 
-public func mconcat<M, S: Monoid where S.M == M>(s: S, t: [M]) -> M {
-	return (t.reduce(s.mzero()) { s.op($0, y: $1) })
+public func mconcat<S: Monoid>(t: [S]) -> S {
+	return sconcat(S.mzero, t)
 }
 
 /// The Monoid of numeric types under addition.
-public struct Sum<A, N: Num where N.N == A> : Monoid {
-	public typealias M = A
+public struct Sum<N: Num> : Monoid {
+    public let value: () -> N
 
-	let n: () -> N // work around for rdar://17109323
-
-	public init(i : @autoclosure () -> N) {
-		n = i
+	public init(_ x: @autoclosure () -> N) {
+		value = x
 	}
 
-	public func mzero() -> M {
-		return n().zero()
+    public static var mzero: Sum<N> {
+		return Sum(N.zero)
 	}
 
-	public func op(x : M, y : M) -> M {
-		return n().add(x, y: y);
-	}
+	public func op(other : Sum<N>) -> Sum<N> {
+		return Sum(value().plus(other.value()))
+    }
 }
 
 /// The Monoid of numeric types under multiplication.
-public struct Product<A, N: Num where N.N == A> : Monoid {
-	public typealias M = A
+public struct Product<N: Num> : Monoid {
+    public let value: () -> N
 
-	let n: () -> N
+    public init(_ x: @autoclosure () -> N) {
+        value = x
+    }
 
-	public init(i : @autoclosure () -> N) {
-		n = i
-	}
+    public static var mzero: Product<N> {
+        return Product(N.one)
+    }
 
-	public func mzero() -> M {
-		return n().succ(n().zero())
-	}
+    public func op(other : Product<N>) -> Product<N> {
+        return Product(value().times(other.value()))
+    }
+}
 
-	public func op(x : M, y : M) -> M {
-		return n().multiply(x, y: y);
-	}
+/// The Semigroup-lifting Maybe Monoid
+public struct AdjoinNil<A: Semigroup> : Monoid {
+    public let value: () -> Maybe<A>
+
+    public init(_ x: @autoclosure () -> Maybe<A>) {
+        value = x
+    }
+
+    public static var mzero: AdjoinNil<A> {
+        return AdjoinNil(Maybe.none())
+    }
+
+    public func op(other: AdjoinNil<A>) -> AdjoinNil<A> {
+        if let x = value().value {
+            if let y = other.value().value {
+                return AdjoinNil(Maybe(x.op(y)))
+            } else {
+                return self
+            }
+        } else {
+            return other
+        }
+    }
+}
+
+/// The left-biased Maybe Monoid.
+public struct First<A: Comparable> : Monoid {
+    public let value: () -> Maybe<A>
+
+    public init(_ x: @autoclosure () -> Maybe<A>) {
+        value = x
+    }
+
+    public static var mzero: First<A> {
+        return First(Maybe.none())
+    }
+
+    public func op(other : First<A>) -> First<A> {
+        if value().isJust() {
+            return self
+        } else {
+            return other
+        }
+    }
+}
+
+/// The right-biased Maybe Monoid.
+public struct Last<A: Comparable> : Semigroup {
+    public let value: () -> Maybe<A>
+
+    public init(_ x: @autoclosure () -> Maybe<A>) {
+        value = x
+    }
+
+    public static var mzero: First<A> {
+        return First(Maybe.none())
+    }
+
+    public func op(other : Last<A>) -> Last<A> {
+        if other.value().isJust() {
+            return other
+        } else {
+            return self
+        }
+    }
 }
 
