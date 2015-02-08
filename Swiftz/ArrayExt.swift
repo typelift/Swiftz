d70becca322522e04f45d9d3a94bd675ad7d6de1
Christian Hoffmann <hffmnn@googlemail.com>
2015-02-08 17:32:16 +0100

    Make tests green again

--- Swiftz/ArrayExt.swift
+++ Swiftz/ArrayExt.swift
@@ -76,12 +76,14 @@ public func find<T>(list : [T], f : (T -> Bool)) -> T? {
 ///     splitAt(4, [1,2,3])     == ([1,2,3],[])
 ///     splitAt(0, [1,2,3])     == ([],[1,2,3])
 public func splitAt<T>(index : Int, list : [T]) -> ([T], [T]) {
-	switch index {
-	case 0..<list.count: 
-		return (Array(list[0..<index]), Array(list[index..<list.count]))
-	case _:
-		return ([T](), [T]())
-	}
+    switch index {
+    case 0..<list.count:
+        return (Array(list[0..<index]), Array(list[index..<list.count]))
+    case list.count...Int.max:
+        return (Array(list[0..<list.count]), [T]())
+    case _:
+        return ([T](), [T]())
+    }
 }
 
 /// Takes a separator and a list and intersperses that element throughout the list.
