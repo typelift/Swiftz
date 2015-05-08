9e63f9f2e2002168e3a6d2a3115dbbb3aa76e614
Robert Widmann <devteam.codafi@gmail.com>
2015-05-07 20:42:47 -0600

    Convert Either's specs to properties

--- SwiftzTests/EitherSpec.swift
+++ SwiftzTests/EitherSpec.swift
@@ -8,42 +8,75 @@
 
 import XCTest
 import Swiftz
+import SwiftCheck
+
+struct EitherOf<A : Arbitrary, B : Arbitrary> : Arbitrary {
+	let getEither : Either<A, B>
+
+	init(_ either : Either<A, B>) {
+		self.getEither = either
+	}
+
+	var description : String {
+		return "\(self.getEither)"
+	}
+
+	private static func create(either : Either<A, B>) -> EitherOf<A, B> {
+		return EitherOf(either)
+	}
+
+	static func arbitrary() -> Gen<EitherOf<A, B>> {
+		return oneOf([ liftM(Either.left)(m1: A.arbitrary()), liftM(Either.right)(m1: B.arbitrary()) ]).fmap(EitherOf.create)
+	}
+
+	static func shrink(bl : EitherOf<A, B>) -> [EitherOf<A, B>] {
+		return bl.getEither.either(onLeft: { x in
+			return A.shrink(x).map(Either.left).map(EitherOf.create)
+		}, onRight: { y in
+			return B.shrink(y).map(Either.right).map(EitherOf.create)
+		})
+	}
+}
 
 class EitherSpec : XCTestCase {
-	func testEither() {
-		func divTwoEvenly(x: Int) -> Either<String, Int> {
-			if x % 2 == 0 {
-				return Either.left("\(x) was div by 2")
+	func divTwoEvenly(x: Int) -> Either<String, Int> {
+		if x % 2 == 0 {
+			return Either.left("\(x) was div by 2")
+		} else {
+			return Either.right(x / 2)
+		}
+	}
+
+	func testProperties() {
+		property["divTwoEvenly"] = forAll { (x : Int) in
+			let first : Either<String, Int> = self.divTwoEvenly(x)
+			return first.either(onLeft: { s in
+				return s == "\(x) was div by 2"
+			}, onRight: { r in
+				return r == (x / 2)
+			})
+		}
+
+		property["fold returns its default on .Left"] = forAll { (e : EitherOf<String, Int>) in
+			if e.getEither.isLeft() {
+				return e.getEither.fold(0, f: identity) == 0
 			} else {
-				return Either.right(x / 2)
+				return true // discard
 			}
 		}
 
+		property["Either is a bifunctor"] = forAll { (e : EitherOf<String, Int>) in
+			let y = EitherBF(e.getEither).bimap(identity, *2)
+			if e.getEither.isLeft() {
+				return y == e.getEither
+			} else {
+				return y == (*2) <^> e.getEither
+			}
+		}
+
+		// TODO: How in hell does this typecheck?
 		// either
 		XCTAssert(Either.left("foo").either(onLeft: { l in l+"!" }, onRight: { r in r+1 }) == "foo!")
 		XCTAssert(Either.right(1).either(onLeft: { l in l+"!" }, onRight: { r in r+1 }) == 2)
-
-		// fold
-		XCTAssert(Either.left("foo").fold(0, f: identity) == 0)
-    XCTAssert(Either<String, Int>.right(10).fold(0, f: identity) == 10)
-
-		// TODO: test <^>, <*> and pure
-
-		let start = 17
-		let first: Either<String, Int> = divTwoEvenly(start)
-		let prettyPrinted: Either<String, String> = { $0.description } <^> first
-		let snd = first >>- divTwoEvenly
-		XCTAssert(prettyPrinted == Either.right("8"))
-		XCTAssert(snd == Either.left("8 was div by 2"))
-	}
-
-	func testEitherBifunctor() {
-		let x : Either<String, Int> = Either.right(2)
-		let y = EitherBF(x).bimap(identity, { $0 * 2 })
-		XCTAssert(y == Either.right(4))
-
-		let a : Either<String, Int> = Either.left("Error!")
-		let b = EitherBF(a).bimap(identity, { $0 * 2 })
-		XCTAssert(b == a);
 	}
 }
