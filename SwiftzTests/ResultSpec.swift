c941b462a13ecb2f7b485c8a35703f7733e76c9c
Robert Widmann <devteam.codafi@gmail.com>
2015-06-01 13:34:48 -0600

    Convert Result tests to SwiftCheck

--- SwiftzTests/ResultSpec.swift
+++ SwiftzTests/ResultSpec.swift
@@ -6,45 +6,112 @@
 //  Copyright (c) 2015 TypeLift. All rights reserved.
 //
 
-import XCTest
 import Swiftz
+import XCTest
+import SwiftCheck
+
+let defaultError = NSError(domain: "Swiftz", code: -1, userInfo: nil)
+
+struct ResultOf<A : Arbitrary> : Arbitrary, Printable {
+	let getResult : Result<A>
+
+	init(_ maybe : Result<A>) {
+		self.getResult = maybe
+	}
+
+	var description : String {
+		return "\(self.getResult)"
+	}
+
+	private static func create(opt : Result<A>) -> ResultOf<A> {
+		return ResultOf(opt)
+	}
+
+	static func arbitrary() -> Gen<ResultOf<A>> {
+		return Gen.frequency([
+			(1, Gen.pure(ResultOf(.Error(defaultError)))),
+			(3, liftM({ ResultOf(.Value(Box($0))) })(m1: A.arbitrary()))
+		])
+	}
+
+	static func shrink(bl : ResultOf<A>) -> [ResultOf<A>] {
+		switch bl.getResult {
+		case .Error(_):
+			return []
+		case let .Value(v):
+			return [ResultOf(.Error(defaultError))] + A.shrink(v.value).map({ ResultOf(.Value(Box($0))) })
+		}
+	}
+}
+
+func == <T : protocol<Arbitrary, Equatable>>(lhs : ResultOf<T>, rhs : ResultOf<T>) -> Bool {
+	return lhs.getResult == rhs.getResult
+}
+
+func != <T : protocol<Arbitrary, Equatable>>(lhs : ResultOf<T>, rhs : ResultOf<T>) -> Bool {
+	return !(lhs == rhs)
+}
 
 class ResultSpec : XCTestCase {
-	func testResult() {
-		let divisionError = NSError(domain: "DivisionDomain", code: 1, userInfo: nil)
-
-		func divTwoEvenly(x: Int) -> Result<Int> {
-			if x % 2 == 0 {
-				return Result.error(divisionError)
-			} else {
-				return Result.value(x / 2)
-			}
+	func testProperties() {
+		property["There is an isomorphism between Either<NSError, A> and Result<A>"] = forAll { (l : ResultOf<Int>) in
+			return l.getResult == l.getResult.toEither().toResult(identity)
 		}
 
-		// fold
-		XCTAssert(Result.error(divisionError).fold(0, f: identity) == 0)
-		XCTAssert(Result.value(10).fold(0, f: identity) == 10)
+		property["Results of Equatable elements obey reflexivity"] = forAll { (l : ResultOf<Int>) in
+			return l == l
+		}
 
-		let start = 17
-		let first: Result<Int> = divTwoEvenly(start)
-		let prettyPrinted: Result<String> = { $0.description } <^> first
-		let snd = first >>- divTwoEvenly
-		XCTAssert(prettyPrinted == Result.value("8"))
-		XCTAssert(snd == .Error(divisionError))
+		property["Results of Equatable elements obey symmetry"] = forAll { (x : ResultOf<Int>, y : ResultOf<Int>) in
+			return (x == y) == (y == x)
+		}
 
-		let startResult: Result<Int> = Result.pure(start)
-		XCTAssert(startResult == Result.value(17))
-		let doubleResult: Result<Int -> Int> = Result.pure({$0 * 2})
-		XCTAssert((doubleResult <*> startResult) == Result.value(34), "test ap: (result, result)")
-		let noF: Result<Int -> Int> = .Error(divisionError)
-		let noX: Result<Int> = snd
-		XCTAssert((noF <*> startResult) == .Error(divisionError), "test ap: (error, result)")
-		XCTAssert((doubleResult <*> noX) == .Error(divisionError), "test ap: (result, error)")
-		XCTAssert((noF <*> noX) == .Error(divisionError), "test ap: (error, error)")
+		property["Results of Equatable elements obey transitivity"] = forAll { (x : ResultOf<Int>, y : ResultOf<Int>, z : ResultOf<Int>) in
+			return (x == y) && (y == z) ==> (x == z)
+		}
 
-		// special constructor
-		XCTAssert(Result(divisionError, 1) == .Error(divisionError), "special Result cons error")
-		XCTAssert(Result(nil, 1) == Result.value(1), "special Result cons value")
+		property["Results of Equatable elements obey negation"] = forAll { (x : ResultOf<Int>, y : ResultOf<Int>) in
+			return (x != y) == !(x == y)
+		}
+
+		property["Result obeys the Functor identity law"] = forAll { (x : ResultOf<Int>) in
+			return (x.getResult.fmap(identity)) == identity(x.getResult)
+		}
+
+		property["Result obeys the Functor composition law"] = forAll { (f : ArrowOf<Int, Int>, g : ArrowOf<Int, Int>, x : ResultOf<Int>) in
+			return ((f.getArrow • g.getArrow) <^> x.getResult) == (x.getResult.fmap(g.getArrow).fmap(f.getArrow))
+		}
+
+		property["Result obeys the Applicative identity law"] = forAll { (x : ResultOf<Int>) in
+			return (Result.pure(identity) <*> x.getResult) == x.getResult
+		}
+
+		property["Result obeys the first Applicative composition law"] = forAll { (fl : ResultOf<ArrowOf<Int8, Int8>>, gl : ResultOf<ArrowOf<Int8, Int8>>, x : ResultOf<Int8>) in
+			let f = fl.getResult.fmap({ $0.getArrow })
+			let g = gl.getResult.fmap({ $0.getArrow })
+			return (curry(•) <^> f <*> g <*> x.getResult) == (f <*> (g <*> x.getResult))
+		}
+
+		property["Result obeys the second Applicative composition law"] = forAll { (fl : ResultOf<ArrowOf<Int8, Int8>>, gl : ResultOf<ArrowOf<Int8, Int8>>, x : ResultOf<Int8>) in
+			let f = fl.getResult.fmap({ $0.getArrow })
+			let g = gl.getResult.fmap({ $0.getArrow })
+			return (Result.pure(curry(•)) <*> f <*> g <*> x.getResult) == (f <*> (g <*> x.getResult))
+		}
+
+		property["Result obeys the Monad left identity law"] = forAll { (a : Int, fa : ArrowOf<Int, ResultOf<Int>>) in
+			let f = { $0.getResult } • fa.getArrow
+			return (Result.pure(a) >>- f) == f(a)
+		}
+
+		property["Result obeys the Monad right identity law"] = forAll { (m : ResultOf<Int>) in
+			return (m.getResult >>- Result.pure) == m.getResult
+		}
+
+		property["Result obeys the Monad associativity law"] = forAll { (fa : ArrowOf<Int, ResultOf<Int>>, ga : ArrowOf<Int, ResultOf<Int>>, m : ResultOf<Int>) in
+			let f = { $0.getResult } • fa.getArrow
+			let g = { $0.getResult } • ga.getArrow
+			return ((m.getResult >>- f) >>- g) == (m.getResult >>- { x in f(x) >>- g })
+		}
 	}
 
 	func testResultFrom() {
@@ -61,18 +128,5 @@ class ResultSpec : XCTestCase {
 
 		XCTAssertTrue((throwableFunction !! -1) == Result.error(e), "")
 		XCTAssertTrue((throwableFunction !! 1) == Result.value("1"), "")
-	}
-
-	func testEitherResult() {
-		// tests:
-		// - either -> result
-		// - result -> either
-
-		let resultOne: Result<Int> = Result.value(1)
-		let eitherOne: Either<NSError, Int> = resultOne.toEither()
-		let resultAgain: Result<Int> = eitherOne.toResult(identity)
-		XCTAssert(resultOne == resultAgain)
-
-		let typeinfworkplz = Result.Value(Box(1)).toEither().toResult(identity)
 	}
 }
