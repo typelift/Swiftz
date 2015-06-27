74cfd25886444434694bf230387becda221add7b
Robert Widmann <devteam.codafi@gmail.com>
2015-06-27 17:01:25 -0600

    Add inits/tails

--- Swiftz/ArrayExt.swift
+++ Swiftz/ArrayExt.swift
@@ -87,6 +87,20 @@ extension Array {
 			return .Some(xs)
 		}
 	}
+	
+	/// Returns an array of all initial segments of the receiver, shortest first
+	public var inits : [[T]] {
+		return self.reduce([[T]](), combine: { xss, x in
+			return xss.map { $0.cons(x) }.cons([])
+		})
+	}
+	
+	/// Returns an array of all final segments of the receiver, longest first
+	public var tails : [[T]] {
+		return self.reduce([[T]](), combine: { x, y in
+			return [x.first!.cons(y)] + x
+		})
+	}
 
 	/// Takes, at most, a specified number of elements from a list and returns that sublist.
 	///
@@ -129,7 +143,6 @@ extension Array {
 			return .Some(x, xs)
 		}
 	}
-
 	/// Safely indexes into an array by converting out of bounds errors to nils.
 	public func safeIndex(i : Int) -> T? {
 		if i < self.count && i >= 0 {
@@ -330,6 +343,50 @@ extension Array {
 			}
 			return []
 		}
+	}
+}
+
+extension Array where T : Equatable {
+	/// Takes two lists and returns true if the first string is a prefix of the second string.
+	public func isPrefixOf(r : [T]) -> Bool {
+		switch (self.match, r.match) {
+		case (.Cons(let x, let xs), .Cons(let y, let ys)) where (x == y):
+			return xs.isPrefixOf(ys)
+		case (.Nil, _):
+			return true
+		default:
+			return false
+		}
+	}
+	
+	/// Takes two lists and returns true if the first string is a suffix of the second string.
+	public func isSuffixOf(r : [T]) -> Bool {
+		return self.reverse().isPrefixOf(r.reverse())
+	}
+	
+	/// Takes two lists and returns true if the first string is contained entirely anywhere in the
+	/// second string.
+	public func isInfixOf(r : [T]) -> Bool {
+		return r.tails.any(self.isPrefixOf)
+	}
+	
+	/// Takes two strings and drops items in the first from the second.  If the first string is not a
+	/// prefix of the second string this function returns Nothing.
+	public func stripPrefix(r : [T]) -> Optional<[T]> {
+		switch (self.match, r.match) {
+		case (.Nil, _):
+			return .Some(r)
+		case (.Cons(let x, let xs), .Cons(let y, _)) where x == y:
+			return xs.stripPrefix(xs)
+		default:
+			return .None
+		}
+	}
+	
+	/// Takes two strings and drops items in the first from the end of the second.  If the first
+	/// string is not a suffix of the second string this function returns nothing.
+	public func stripSuffix(r : [T]) -> Optional<[T]> {
+		return self.reverse().stripPrefix(r.reverse()).map({ $0.reverse() })
 	}
 }
 
