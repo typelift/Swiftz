f29b972df855fe9b330686b2ec5f12df41a94531
Robert Widmann <devteam.codafi@gmail.com>
2015-07-02 18:34:39 -0600

    Update to latest SwiftCheck

--- SwiftzTests/StateSpec.swift
+++ SwiftzTests/StateSpec.swift
@@ -8,6 +8,31 @@
 
 import XCTest
 import Swiftz
+import SwiftCheck
+
+struct StateOf<S, A : Arbitrary> : Arbitrary, CustomStringConvertible {
+    let getState : State<S, A>
+    
+    init(_ state : State<S, A>) {
+        self.getState = state
+    }
+    
+    var description : String {
+        return "\(self.getState)"
+    }
+    
+    private static func create(opt : State<S, A>) -> StateOf<S, A> {
+        return StateOf(opt)
+    }
+    
+    static func arbitrary() -> Gen<StateOf<S, A>> {
+        fatalError()
+    }
+    
+    static func shrink(bl : StateOf<S, A>) -> [StateOf<S, A>] {
+        return []
+    }
+}
 
 class StateSpec : XCTestCase {
 }
