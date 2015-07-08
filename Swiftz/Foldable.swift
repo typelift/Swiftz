31c3264c517423363e033f3f11104af95a118fec
Robert Widmann <devteam.codafi@gmail.com>
2015-07-08 15:30:18 -0600

    Document foldable

--- Swiftz/Foldable.swift
+++ Swiftz/Foldable.swift
@@ -6,11 +6,17 @@
 //  Copyright © 2015 TypeLift. All rights reserved.
 //
 
+/// A `Foldable` type admits a way of "summarizing" its entire structure.
 protocol Foldable {
 	typealias A
 	typealias B
 
-	func foldr(_ : A -> B -> B, _ : B) -> B
-	func foldl(_ : B -> A -> B, _ : B) -> B
-	func foldMap<M : Monoid>(_ : A -> M) -> M
+	/// Summarizes the receiver right-associatively.
+	func foldr(folder : A -> B -> B, _ initial : B) -> B
+	
+	/// Summarizes the receiver left-associatively.
+	func foldl(folder : B -> A -> B, _ initial : B) -> B
+	
+	/// Map each element of the receiver to a monoid, and combine the results.
+	func foldMap<M : Monoid>(map : A -> M) -> M
 }
