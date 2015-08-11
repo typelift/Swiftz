4658434b72726646ba719b513a0b80ebb056fca8
Robert Widmann <devteam.codafi@gmail.com>
2015-08-11 13:53:35 -0600

    Simplify specs with WitnessedArbitrary

--- SwiftzTests/ListSpec.swift
+++ SwiftzTests/ListSpec.swift
@@ -10,59 +10,69 @@ import XCTest
 import Swiftz
 import SwiftCheck
 
-/// Generates an array of arbitrary values of type A.
-extension List where A : Arbitrary {
-	public static var arbitrary : Gen<List<A>> {
-		return List.init <^> [A].arbitrary
+/// Generates an array of arbitrary values.
+extension List where Element : Arbitrary {
+	public static var arbitrary : Gen<List<Element>> {
+		return List.init <^> [Element].arbitrary
 	}
 	
-	public static func shrink(xs : List<A>) -> [List<A>] {
-		return List.init <^> [A].shrink(xs.map(identity))
+	public static func shrink(xs : List<Element>) -> [List<Element>] {
+		return List.init <^> [Element].shrink(xs.map(identity))
+	}
+}
+
+extension List : WitnessedArbitrary {
+	public typealias Param = Element
+
+	public static func forAllWitnessed<A : Arbitrary>(wit : A -> Element)(pf : (List<Element> -> Testable)) -> Property {
+		return forAllShrink(List<A>.arbitrary, shrinker: List<A>.shrink, f: { bl in
+			return pf(bl.map(wit))
+		})
 	}
 }
 
 class ListSpec : XCTestCase {
 	func testProperties() {
-		property("Lists of Equatable elements obey reflexivity") <- forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { l in
+		property("Lists of Equatable elements obey reflexivity") <- forAll { (l : List<Int>) in
 			return l == l
 		}
 		
-		property("Lists of Equatable elements obey symmetry") <- forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { x in
-			return forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { y in
+		property("Lists of Equatable elements obey symmetry") <- forAll { (x : List<Int>) in
+			return forAll { (y : List<Int>) in
 				return (x == y) == (y == x)
 			}
 		}
 
-		property("Lists of Equatable elements obey transitivity") <- forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { x in
-			return forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { y in
-				let inner = forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { z in
+		property("Lists of Equatable elements obey transitivity") <- forAll { (x : List<Int>) in
+			return forAll { (y : List<Int>) in
+				let inner = forAll { (z : List<Int>) in
 					return (x == y) && (y == z) ==> (x == z)
 				}
 				return inner
 			}
 		}
 
-		property("Lists of Equatable elements obey negation") <- forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { x in
-			return forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { y in
+		property("Lists of Equatable elements obey negation") <- forAll { (x : List<Int>) in
+			return forAll { (y : List<Int>) in
 				return (x != y) == !(x == y)
 			}
 		}
 
-		property("Lists of Comparable elements obey reflexivity") <- forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { l in
+		property("Lists of Comparable elements obey reflexivity") <- forAll { (l : List<Int>) in
 			return l == l
 		}
 
-		property("List obeys the Functor identity law") <- forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { x in
+		property("List obeys the Functor identity law") <- forAll { (x : List<Int>) in
 			return (x.fmap(identity)) == identity(x)
 		}
 
 		property("List obeys the Functor composition law") <- forAll { (f : ArrowOf<Int, Int>, g : ArrowOf<Int, Int>) in
-			return forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { x in
+			return forAll { (x : List<Int>) in
 				return ((f.getArrow • g.getArrow) <^> x) == (x.fmap(g.getArrow).fmap(f.getArrow))
 			}
 		}
 
-		property("List obeys the Applicative identity law") <- forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { x in
+		property("List obeys the Applicative identity law") <- forAll { (x : List<Int>) in
 			return (List.pure(identity) <*> x) == x
 		}
 
@@ -91,15 +101,15 @@ class ListSpec : XCTestCase {
 			}
 		}
 
-		property("List obeys the Monoidal left identity law") <- forAllShrink(List<Int8>.arbitrary, shrinker: List<Int8>.shrink) { x in
+		property("List obeys the Monoidal left identity law") <- forAll { (x : List<Int8>) in
 			return (x <> List()) == x
 		}
 
-		property("List obeys the Monoidal right identity law") <- forAllShrink(List<Int8>.arbitrary, shrinker: List<Int8>.shrink) { x in
+		property("List obeys the Monoidal right identity law") <- forAll { (x : List<Int8>) in
 			return (List() <> x) == x
 		}
 
-		property("List can cycle into an infinite list") <- forAllShrink(List<Int8>.arbitrary, shrinker: List<Int8>.shrink) { x in
+		property("List can cycle into an infinite list") <- forAll { (x : List<Int8>) in
 			return !x.isEmpty ==> {
 				let cycle = x.cycle()
 
@@ -109,33 +119,33 @@ class ListSpec : XCTestCase {
 			}
 		}
 
-		property("isEmpty behaves") <- forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { xs in
+		property("isEmpty behaves") <- forAll { (xs : List<Int>) in
 			return xs.isEmpty == (xs.count == 0)
 		}
 
-		property("map behaves") <- forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { xs in
+		property("map behaves") <- forAll { (xs : List<Int>) in
 			return xs.map(+1) == xs.fmap(+1)
 		}
 
-		property("map behaves") <- forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { xs in
+		property("map behaves") <- forAll { (xs : List<Int>) in
 			let fs = { List<Int>.replicate(2, value: $0) }
 			return (xs >>- fs) == xs.map(fs).reduce(+, initial: List())
 		}
 
 		property("filter behaves") <- forAll { (pred : ArrowOf<Int, Bool>) in
-			return forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { xs in
+			return forAll { (xs : List<Int>) in
 				return xs.filter(pred.getArrow).reduce({ $0.0 && pred.getArrow($0.1) }, initial: true)
 			}
 		}
 
 		property("take behaves") <- forAll { (limit : UInt) in
-			return forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { xs in
+			return forAll { (xs : List<Int>) in
 				return xs.take(limit).count <= limit
 			}
 		}
 
 		property("drop behaves") <- forAll { (limit : UInt) in
-			return forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { xs in
+			return forAll { (xs : List<Int>) in
 				let l = xs.drop(limit)
 				if xs.count >= limit {
 					return l.count == (xs.count - limit)
@@ -144,7 +154,7 @@ class ListSpec : XCTestCase {
 			}
 		}
 
-		property("scanl behaves") <- forAllShrink(List<Int>.arbitrary, shrinker: List<Int>.shrink) { withArray in
+		property("scanl behaves") <- forAll { (withArray : List<Int>) in
 			let scanned = withArray.scanl(curry(+), initial: 0)
 			switch withArray.match {
 			case .Nil:
