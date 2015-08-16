4c230bd85e9df82f65bf7329135a6b6cd1ad73df
Robert Widmann <devteam.codafi@gmail.com>
2015-08-16 12:24:17 -0600

    Proper documentation for arrow extensions

--- Swiftz/ArrowExt.swift
+++ Swiftz/ArrowExt.swift
@@ -20,21 +20,30 @@ public func >>> <A, B, C>(f : (A -> B), g : (B -> C)) -> (A -> C) {
 
 // MARK: - Data.Functor
 
-/// Fmap | Function composition.
+/// Fmap | Returns a function that applies the given transformation to any arguments drawn from its 
+/// environment.
+///
+/// Function composition.
 public func <^> <A, B, R>(f : A -> B, g : R -> A) -> (R -> B) {
 	return f • g
 }
 
 // MARK: - Control.Applicative
 
-/// Ap | "Share Environment"
+/// Ap | Uses the latter function to draw an argument from the environment that is then applied to 
+/// the former function.
+///
+/// "Share Environment"
 public func <*> <A, B, R>(f : (R -> (A -> B)), g : (R -> A)) -> (R -> B) {
 	return { x in f(x)(g(x)) }
 }
 
 // MARK: - Control.Monad
 
-/// Bind | "Kontinue Environment"
+/// Bind | Draws a value from the environment, applies it to the continuation, then returns the
+/// result of the application.
+///
+/// "Kontinue Environment"
 public func >>- <A, B, R>(f : (R -> A), k : (A -> (R -> B))) -> (R -> B) {
 	return { r in k(f(r))(r) }
 }
