bf10b34a1553a285f33cc84e061160555019de3a
Robert Widmann <devteam.codafi@gmail.com>
2015-08-19 22:13:33 -0400

    Restructure docs

--- Swiftz/Stream.swift
+++ Swiftz/Stream.swift
@@ -14,9 +14,9 @@ public struct Stream<T> {
 		self.step = step
 	}
 	
-	/// Uses function to construct a Stream.
+	/// Uses function to construct a `Stream`.
 	///
-	/// Unlike unfold for lists, unfolds to construct a Stream have no base case.
+	/// Unlike unfold for lists, unfolds to construct a `Stream` have no base case.
 	public static func unfold<A>(initial : A, _ f : A -> (T, A)) -> Stream<T> {
 		let (x, d) = f(initial)
 		return Stream { (x, Stream.unfold(d, f)) }
@@ -27,12 +27,12 @@ public struct Stream<T> {
 		return Stream { (x, `repeat`(x)) }
 	}
 	
-	/// Returns a Stream of an infinite number of iteratations of applications of a function to a value.
+	/// Returns a `Stream` of an infinite number of iteratations of applications of a function to a value.
 	public static func iterate(initial : T, _ f : T -> T)-> Stream<T> {
 		return Stream { (initial, Stream.iterate(f(initial), f)) }
 	}
 	
-	/// Cycles a non-empty list into an infinite Stream of repeating values.
+	/// Cycles a non-empty list into an infinite `Stream` of repeating values.
 	///
 	/// This function is partial with respect to the empty list.
 	public static func cycle(xs : [T]) -> Stream<T> {
@@ -48,7 +48,7 @@ public struct Stream<T> {
 		return index(self)(n)
 	}
 	
-	/// Looks up the nth element of a Stream.
+	/// Looks up the nth element of a `Stream`.
 	public func index<T>(s : Stream<T>) -> UInt -> T {
 		return { n in
 			if n == 0 {
@@ -58,27 +58,27 @@ public struct Stream<T> {
 		}
 	}
 	
-	/// Returns the first element of a Stream.
+	/// Returns the first element of a `Stream`.
 	public var head : T {
 		return self.step().head
 	}
 	
-	/// Returns the remaining elements of a Stream.
+	/// Returns the remaining elements of a `Stream`.
 	public var tail : Stream<T> {
 		return self.step().tail
 	}
 	
-	/// Returns a Stream of all initial segments of a Stream.
+	/// Returns a `Stream` of all initial segments of a `Stream`.
 	public var inits : Stream<[T]> {
 		return Stream<[T]> { ([], self.step().tail.inits.fmap({ $0.cons(self.step().head) })) }
 	}
 	
-	/// Returns a Stream of all final segments of a Stream.
+	/// Returns a `Stream` of all final segments of a `Stream`.
 	public var tails : Stream<Stream<T>> {
 		return Stream<Stream<T>> { (self, self.step().tail.tails) }
 	}
 	
-	/// Returns a pair of the first n elements and the remaining eleemnts in a Stream.
+	/// Returns a pair of the first n elements and the remaining eleemnts in a `Stream`.
 	public func splitAt(n : UInt) -> ([T], Stream<T>) {
 		if n == 0 {
 			return ([], self)
@@ -87,7 +87,7 @@ public struct Stream<T> {
 		return (p.cons(self.head), r)
 	}
 	
-	/// Returns the longest prefix of values in a Stream for which a predicate holds.
+	/// Returns the longest prefix of values in a `Stream` for which a predicate holds.
 	public func takeWhile(p : T -> Bool) -> [T] {
 		if p(self.step().head) {
 			return self.step().tail.takeWhile(p).cons(self.step().head)
@@ -103,7 +103,7 @@ public struct Stream<T> {
 		return self
 	}
 	
-	/// Returns the first n elements of a Stream.
+	/// Returns the first n elements of a `Stream`.
 	public func take(n : UInt) -> [T] {
 		if n == 0 {
 			return []
@@ -111,7 +111,7 @@ public struct Stream<T> {
 		return self.step().tail.take(n - 1).cons(self.step().head)
 	}
 	
-	/// Returns a Stream with the first n elements removed.
+	/// Returns a `Stream` with the first n elements removed.
 	public func drop(n : UInt) -> Stream<T> {
 		if n == 0 {
 			return self
@@ -119,7 +119,7 @@ public struct Stream<T> {
 		return self.step().tail.tail.drop(n - 1)
 	}
 
-	/// Removes elements from the Stream that do not satisfy a given predicate.
+	/// Removes elements from the `Stream` that do not satisfy a given predicate.
 	///
 	/// If there are no elements that satisfy this predicate this function will diverge.
 	public func filter(p : T -> Bool) -> Stream<T> {
@@ -129,22 +129,22 @@ public struct Stream<T> {
 		return self.step().tail.filter(p)
 	}
 	
-	/// Returns a Stream of alternating elements from each Stream.
+	/// Returns a `Stream` of alternating elements from each Stream.
 	public func interleaveWith(s2 : Stream<T>) -> Stream<T> {
 		return Stream { (self.step().head, s2.interleaveWith(self.tail)) }
 	}
 	
-	/// Creates a Stream alternating an element in between the values of another Stream.
+	/// Creates a `Stream` alternating an element in between the values of another Stream.
 	public func intersperse(x : T) -> Stream<T> {
 		return Stream { (self.step().head, Stream { (x, self.step().tail.intersperse(x)) } ) }
 	}
 
-	/// Returns a Stream of successive reduced values.
+	/// Returns a `Stream` of successive reduced values.
 	public func scanl<A>(initial : A, combine : A -> T -> A) -> Stream<A> {
 		return Stream<A> { (initial, self.step().tail.scanl(combine(initial)(self.step().head), combine: combine)) }
 	}
 
-	/// Returns a Stream of successive reduced values.
+	/// Returns a `Stream` of successive reduced values.
 	public func scanl1(f : T -> T -> T) -> Stream<T> {
 		return self.step().tail.scanl(self.step().head, combine: f)
 	}
@@ -157,17 +157,17 @@ public func transpose<T>(ss : Stream<Stream<T>>) -> Stream<Stream<T>> {
 	return Stream { (Stream { (xs.step().head, yss.fmap{ $0.head }) }, transpose(Stream { (xs.step().tail, yss.fmap{ $0.tail }) } )) }
 }
 
-/// Zips two Streams into a Stream of pairs.
+/// Zips two `Stream`s into a `Stream` of pairs.
 public func zip<A, B>(s1 : Stream<A>) -> Stream<B> -> Stream<(A, B)> {
 	return { s2 in Stream { ((s1.step().head, s2.step().head), zip(s1.step().tail)(s2.step().tail)) } }
 }
 
-/// Zips two Streams into a third Stream using a combining function.
+/// Zips two `Stream`s into a third Stream using a combining function.
 public func zipWith<A, B, C>(f : A -> B -> C) -> Stream<A> -> Stream<B> -> Stream<C> {
 	return { s1 in { s2 in Stream { (f(s1.step().head)(s2.step().head), zipWith(f)(s1.step().tail)(s2.step().tail)) } } }
 }
 
-/// Unzips a Stream of pairs into a pair of Streams.
+/// Unzips a `Stream` of pairs into a pair of Streams.
 public func unzip<A, B>(sp : Stream<(A, B)>) -> (Stream<A>, Stream<B>) {
 	return (sp.fmap(fst), sp.fmap(snd))
 }
